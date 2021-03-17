86b69b98246fb57684fa08655889d446d9754fd8
tanjataylor1 <78738562+tanjataylor1@users.noreply.github.com>
2021-03-17 19:58:51 -0400

    Initial commit

---
+++ src/sequelize.ts
@@ -0,0 +1,13 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+console.log(config)
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+  'logging':console.log,
+  'dialect': config.dialect,
+  'storage': ':memory:',
+});
